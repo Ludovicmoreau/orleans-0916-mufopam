41ff4246bca9c91c105a2aef9fe4c6cd441d4f31
ludovicmoreau <ludovic_moreau@laposte.net>
2016-09-30 10:32:11 +0200

    devut index, probleme image

--- src/index.php
+++ src/index.php
@@ -5,3 +5,74 @@
  * Date: 30/09/16
  * Time: 09:16
  */
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Page d'acceuil GDR3625</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="banniere
+                    col-lg-12">
+            <img src="" alt="banniere_cnrs">
+        </div>
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="" alt="logoGDR"></a>
+            </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Acceuil <span class="sr-only">(current)</span></a></li>
+                        <li class="active"><a href="#">Equipes </a></li>
+                        <li class="active"><a href="#">News </a></li>
+                        <li class="active"><a href="#">Publication </a></li>
+                        <li class="active"><a href="#">Emploi </a></li>
+                        <li><a href="#"></a></li>
+
+                    </ul>
+                    <form class="navbar-form navbar-left">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+
+
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+
+
+
+
+
+    </div>
+
+
+
+
+</body>
+
+
+
+
+
+</html>
