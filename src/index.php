35aec0174c1c53a93aa6b2d239c79a188f068e6c
ludovicmoreau <ludovic_moreau@laposte.net>
2016-09-30 14:03:43 +0200

    Sauvegarde

--- src/index.php
+++ src/index.php
@@ -8,20 +8,21 @@
 ?>
 
 <html>
-<head>
-    <meta charset="utf-8">
-    <title>Page d'acceuil GDR3625</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>Page d'acceuil GDR3625</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
 
-<body>
+    <body>
         <div class="banniere">
-            <img src="../web/images/banniere.jpg" alt="banniere_cnrs" height="250px" >
+            <img src="../web/images/banniere.jpg" alt="banniere_cnrs" height="250px" class="img-responsive"  >
         </div>
 
         <nav class="navbar navbar-default">
             <div class="container-fluid">
+
 
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -30,13 +31,13 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#"><img src="../web/images/logoGDR.png" alt="logoGDR" height="150px" width="400px"></a>
+                    <a class="navbar-brand" href="#"><img src="../web/images/logoGDRdetour.png" alt="logoGDR" height="90px" width="350px"  ></a>
             </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Acceuil <span class="sr-only">(current)</span></a></li>
+                        <li class="active"><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
                         <li class="active"><a href="#">Equipes </a></li>
                         <li class="active"><a href="#">News </a></li>
                         <li class="active"><a href="#">Publication </a></li>
@@ -44,26 +45,77 @@
                         <li><a href="#"></a></li>
 
                     </ul>
-                    <form class="navbar-form navbar-left">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                    </form>
+
                     <ul class="nav navbar-nav navbar-right">
 
-
                     </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><img src="../web/images/icon_mdp.jpg" alt="connexion" height="15px" width="15px" class="img-responsive"></a></li>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
         </nav>
 
+            <div class="container-fluid ">
+                <div class="row-présentation col-lg-offset-1 col-lg-6 ">
+
+                <h1>Présentation du GDR</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ad adipisci autem deserunt dolores earum eos excepturi harum iste minima mollitia necessitatibus,
+                obcaecati quaerat quisquam quos recusandae soluta veritatis voluptas voluptatum.
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Ad adipisci autem deserunt dolores earum eos excepturi harum iste minima mollitia necessitatibus,
+                 obcaecati quaerat quisquam quos recusandae soluta veritatis voluptas voluptatum.   </p>
+
+                <img src="../web/images/france.png" alt="Carte de france interactive" class="img-responsive">
+                </div>
+
+
+              <div class="row-news col-lg-4">
+                  <a href="#" class="thumbnail" >
+                      <h1>Dernière news</h1>
+                  </a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Architecto cumque dolore ex numquam provident quam rem voluptate.
+                          Blanditiis consequuntur id illo minus
+                          modi perspiciatis qui. At perferendis tempore voluptatem voluptates.</p>
+               </div>
+                <div class="row-news col-lg-4">
+                  <a href="#" class="thumbnail">
+                      <h1>Dernière publication</h1>
+                  </a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Architecto cumque dolore ex numquam provident quam rem voluptate.
+                          Blanditiis consequuntur id illo minus
+                          modi perspiciatis qui. At perferendis tempore voluptatem voluptates.</p>
+                </div>
+                <div class="row-news col-lg-4">
+                  <a href="#" class="thumbnail">
+                      <h1>Dernière offre</h1>
+                  </a>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                          Architecto cumque dolore ex numquam provident quam rem voluptate.
+                          Blanditiis consequuntur id illo minus
+                          modi perspiciatis qui. At perferendis tempore voluptatem voluptates.</p>
+
+
+              </div>
 
 
 
+        <footer>
+            <div class="row-footer col-lg-6">
+                <br/>
+            <a>Mention légales</a>
+            </div>
+            <div class="WCS col-lg-offset-4 col-lg-2">
+                <img src="../web/images/logo_wcs.jpg" alt="logo WCS" height="100px" width="100px" class="img-responsive">
+            </div>
+
+        </footer>
+        </div>
 
 
-</body>
+    </body>
 
 
 
