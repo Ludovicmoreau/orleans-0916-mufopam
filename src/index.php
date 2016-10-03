fb736de7fd378a2471e60b911262e4cd036ff4fb
ludovicmoreau <ludovic_moreau@laposte.net>
2016-10-03 17:22:36 +0200

    Sauvegarde

--- src/index.php
+++ src/index.php
@@ -112,7 +112,7 @@
                     <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
                         <div class="modal-dialog modal-sm" role="document">
                             <div class="modal-content">
-                                ...
+                                Mentions légales
                             </div>
                         </div>
                     </div>
@@ -122,7 +122,7 @@
                     <div class="modal fade bs-example-modal-sm2" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
                         <div class="modal-dialog modal-sm" role="document">
                             <div class="modal-content">
-                                ...
+                                Celine LANDON
                             </div>
                         </div>
                     </div>
