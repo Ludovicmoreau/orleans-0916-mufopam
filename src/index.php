75635d21734e10e4517b33fd308c7e0374d41125
ludovicmoreau <ludovic_moreau@laposte.net>
2016-10-03 11:04:59 +0200

    Sauvegarde pour branche

--- src/index.php
+++ src/index.php
@@ -38,7 +38,7 @@
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
+                    <ul class="nav navbar-nav ">
                         <li class="active"><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
                         <li ><a href="#">Equipes </a></li>
                         <li ><a href="#">News </a></li>
