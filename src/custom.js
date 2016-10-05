2989614f42e9c9e5b3db0b522bd6d327ba262982
MkLHX <mickael.lehoux@gmail.com>
2016-10-05 17:19:23 +0200

    ajouter présentation du groupe

--- src/custom.js
+++ src/custom.js
@@ -1,11 +1,22 @@
 $(function() {
     /*##### Animation scrolling sticky navbar ####*/
-    $(document).on( 'scroll', function(){
+    /*$(document).on( 'scroll', function(){
         console.log('scroll top : ' + $(window).scrollTop());
         if($(window).scrollTop()>=$('.banniere').height()){
             $('nav').addClass('navbar-fixed-top');
         } else if ($(window).scrollTop()<$('.banniere').height()) {
             $('nav').removeClass('navbar-fixed-top');
         }
+    });*/
+    $('nav').affix({
+        offset: {
+            top: $('header').height()
+        }
     });
+
+    /*$('nav').affix({
+        offset: {
+            top: 250
+        }
+    });*/
 });
