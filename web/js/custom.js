71013e307f7d7c8ced5f323849cf18e09a8bff3d
MkLHX <mickael.lehoux@gmail.com>
2016-10-12 18:44:47 +0200

    modifications sticky map and navabar css style

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,13 +1,11 @@
 $(function() {
     /*##### un Sticky map at end of row2 ####*/
-    //alert($(window).height()-($('.row3').height()+ $('footer').height()));
     $(window).scroll(function (event) {
         var scroll = $(window).scrollTop();
-        //console.log(scroll);
-        console.log($('row3').position());
-        if (scroll > $(window).height() - ($('.row3').height() + $('footer').height())) {
-            alert($(window).position() - ($('.row3').height() + $('footer').height()));
-            //$('#map').removeClass('affix');
+        if (scroll > $(window).height() - ($('.row3').height() + $('footer').height() + $('#map').height()+100)) {
+            $('#map').removeClass('affix');
+        } else if (scroll > ($('header').height())) {
+            $('#map').addClass('affix');
         }
     });
 });
